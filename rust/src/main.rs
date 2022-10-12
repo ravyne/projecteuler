009dbbe6b338bfbf157cafe74e150b739227ecef
Michael Thompson <ravyne2001@hotmail.com>
2022-10-12 01:31:28 -0700

    P0002/rust: Add simple, take_while, and iterator-based solutions.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,11 +1,21 @@
 mod p0001;
+mod p0002;
 
-pub use crate::p0001::solutions;
+//pub use crate::p0001::solutions;
+pub use crate::p0002::solutions;
 
 fn main() {
     println!("Starting Project Euler...");
+
     println!("  Problem #1: Multiples of 3s or 5s...");
     println!("    Solution #1.1: simple -- {}", p0001::solutions::simple());
+    println!();
+
+    println!("  Problem #2: Even Fibonacci numbers...");
+    println!("    Solution #2.1: simple -- {}", p0002::solutions::simple());
+    println!("    Solution #2.2: take_while -- {}", p0002::solutions::using_take_while());
+    println!("    Solution #2.3: iterators -- {}", p0002::solutions::using_iterators());
+    println!();
 
     println!("----------");
     println!("Complete.");
